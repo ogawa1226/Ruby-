3dfcffc34b38b4f45b1d0836041f679ef5c41f92
Ogawa <145567537+ogawa1226@users.noreply.github.com>
2024-02-03 11:57:22 +0000

    型

---
+++ curriculum/basic_learning_2.rb
@@ -0,0 +1,20 @@
+#整数型(String)
+puts 5
+
+#文字列型(Integer)
+puts "5"
+
+
+#整数型(String)と文字列型(Integer)の違い
+
+  #整数型(String)
+  puts 5 + 3
+  
+  #文字列型(Integer)
+  puts "5 + 3"
+  
+  #文字列型(Integer)の統合
+  puts "5" + "3"
+  
+#文字列型の統合
+puts "I" + "am" + "Sam"
