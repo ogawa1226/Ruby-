1b437eeb11e9eb637eef59a0f2d077c023715532
Ogawa <145567537+ogawa1226@users.noreply.github.com>
2024-03-04 07:16:55 +0000

    elsif・else

---
+++ ruby/basic_learning_12.rb
@@ -0,0 +1,24 @@
+#elsif
+
+apple = "Nagano"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません"
+end
+
+
+#else
+
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません"
+end
